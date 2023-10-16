c93d0b8ea4fec7c20309fbce4e0bcb737f510430
savioanjos2020@gmail.com <savioanjos2020@gmail.com>
2023-10-16 17:53:36 -0300

    :sparkles: feat: Add service for user registration

---
+++ src/app/pages/register/register.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, take } from 'rxjs';
+import { IUser } from 'src/app/interfaces/user.interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class RegisterService {
+  private readonly API: string = 'http://localhost:8080';
+
+  constructor(private http: HttpClient) {}
+
+  public createUser(user: IUser): Observable<IUser> {
+    return this.http.post<IUser>(this.API, user).pipe(take(1));
+  }
+}
